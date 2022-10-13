61cbb4a2652d0a8c7c1f34263b3a87989287a4cd
Idris Mustapha <idrismustaphaobadaki777@gmail.com>
2022-10-13 14:42:48 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
-#!/bin/bash
-
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * main- Entry
- * Return: Always zero
+ * main - Entry Point
+ *
+ * Return: Always 0(Success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
