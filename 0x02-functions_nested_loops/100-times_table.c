a5435765bfb0c0d5904b829b0fda1cd352e16c37
Idris Mustapha <idrismustaphaobadaki777@gmail.com>
2022-10-18 18:56:02 -0700

    100-103-task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,8 +22,7 @@ void print_times_table(int n)
 				prod = num * mult;
 				if (prod <= 99)
 					_putchar(' ');
-				if (prod <= 99)
-					_putchar(' ');
+
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
