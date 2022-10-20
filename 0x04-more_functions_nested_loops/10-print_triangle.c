e64b9e83c9c1a8fa2a2ee0825a2c0c50df05f948
Idris Mustapha <idrismustaphaobadaki777@gmail.com>
2022-10-20 15:06:02 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_triangle - function to print a triangle
+ * @size: triangle to be printed
+ */
+void print_triangle(int size)
+{
+	int vertical;
+	int horiz;
+
+	if (size > 0)
+	{
+		for (vertical = 0; vertical <= size; vertical++)
+		{
+			for (horiz = size - vertical; horiz > 0; horiz--)
+				_putchar(' ');
+
+			for (horiz = 0; horiz < vertical; horiz++)
+				_putchar('#');
+			if (vertical == size)
+				continue;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
