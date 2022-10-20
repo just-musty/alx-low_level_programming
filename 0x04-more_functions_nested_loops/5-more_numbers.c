2e55bd1b73e38c254a87dfeb322d1ecd14451a46
Idris Mustapha <idrismustaphaobadaki777@gmail.com>
2022-10-20 09:47:17 -0700

    0-5-task

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,12 +10,12 @@ void more_numbers(void)
 
 	for (count = 0; count <= 9; count++)
 	{
-		for (num = 0; num <= 14)
+		for (num = 0; num <= 14; num++)
 		{
-			if (num > 0)
+			if (num > 9)
 				_putchar((num / 10) + '0');
 			_putchar((num % 10) + '0');
 		}
-g	_putchar('\n');
+		_putchar('\n');
 	}
 }
