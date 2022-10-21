26ae8ab9d087a89ed0086dc53fb8c1d265202d08
Idris Mustapha <idrismustaphaobadaki777@gmail.com>
2022-10-21 04:17:21 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - print a square
+ * @size: print size of the square
+ */
+void print_square(int size)
+{
+	int length;
+	int breath;
+
+	if (size > 0)
+	{
+		for (length = 0; length < size; length++)
+		{
+			for (breath = 0; breath < length; breath++)
+				_putchar('#');
+
+			if (length == size - 1)
+				continue;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
