c59694ebe57e48c8047e85a5ad66c580bd22dc62
Idris Mustapha <idrismustaphaobadaki777@gmail.com>
2022-10-27 04:43:56 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strncat - Concatenates two strings using at most
+ *            an inputted number of bytes from src.
+ * @dest: The string to be appended upon.
+ * @src: The string to be appended to dest.
+ * @n: The number of bytes from src to be appended to dest.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, p;
+
+	p = 0;
+	while (dest[i++])
+		p++;
+	for (i = 0; i < n && src[i]; i++)
+		dest[p++] = src[i};
+	return (dest);
+}
