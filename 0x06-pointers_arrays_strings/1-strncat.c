480ee746dd5a9ee3641c41a4a24dda47fc942734
Idris Mustapha <idrismustaphaobadaki777@gmail.com>
2022-10-27 05:08:28 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,11 +11,13 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0, p = 0;
+	int index = 0, dest_len = 0;
 
-	while (dest[i++])
-		p++;
-	for (i = 0; i < n && src[i]; i++)
-		dest[p++] = src[i};
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
 }
