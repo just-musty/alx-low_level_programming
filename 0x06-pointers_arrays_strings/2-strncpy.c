6c2e1a3fdfca5f108ca45cecee7a36b20dad43de
Idris Mustapha <idrismustaphaobadaki777@gmail.com>
2022-10-27 05:43:21 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strncpy - Copies at most an inputted number
+ * *_strncpy - Copies at most an inputted number
  *            of bytes from string src into dest.
  * @dest: The buffer storing the string copy.
  * @src: The source string.
@@ -11,14 +11,16 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, p;
+	int index = 0, src_len = 0;
 
-	p = 0;
-	while (dest[i++])
-		p++;
-	for (i = 0; src[i] && i < n; i++)
-		dest[i] = src[i];
-	for (i = p; i < n; i++)
-		dest[i] = '\0';
+	while (src[index++])
+		src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+
 	return (dest);
 }
