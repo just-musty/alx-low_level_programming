ac21352ba48f20d5bddada3fd1b7eec4b8cf7460
Idris Mustapha <idrismustaphaobadaki777@gmail.com>
2022-10-27 04:51:03 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *_strncpy - Copies at most an inputted number
+ * _strncpy - Copies at most an inputted number
  *            of bytes from string src into dest.
  * @dest: The buffer storing the string copy.
  * @src: The source string.
