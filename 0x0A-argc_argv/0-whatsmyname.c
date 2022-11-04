4dd84cea757c4dffa3e8526b682232f02d6b666e
Idris Mustapha <idrismustaphaobadaki777@gmail.com>
2022-11-04 08:51:52 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: An array of pointers to the arguments.
  * Return: always 0.
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
