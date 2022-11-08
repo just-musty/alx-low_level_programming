f8999cef058dd856ca1dde248e30a2708c132529
Idris Mustapha <idrismustaphaobadaki777@gmail.com>
2022-11-08 13:29:43 -0800

    101-strtow.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,7 +22,7 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; s1[i] || s2[i]; i++)
 		p++;
 
-	conc = malloc(sizeof(char) * p);
+	conc = (char *)malloc(sizeof(char) * p);
 
 	if (conc == NULL)
 		return (NULL);
