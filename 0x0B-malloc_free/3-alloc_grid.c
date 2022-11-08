d5971805b5382871f1eaafcfcb3eacee21d8950c
Idris Mustapha <idrismustaphaobadaki777@gmail.com>
2022-11-08 12:58:53 -0800

    4-free_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -29,7 +29,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		alloc[i] = malloc(width * sizeof(int);
+		alloc[i] = malloc(width * sizeof(int));
 
 		if (alloc[i] == NULL)
 		{
