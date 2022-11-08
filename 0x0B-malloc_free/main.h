bd3a4d6592ab37fc8feb2eccaeb3153677a63123
Idris Mustapha <idrismustaphaobadaki777@gmail.com>
2022-11-08 02:11:57 -0800

    4-free_grid.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,7 +5,7 @@
  * File: main.h
  *
  * Desc: Header file containing prototypes for all functions
- *       used in the 0x0A-malloc_free directory.
+ *       used in the 0x0B-malloc_free directory.
  */
 
 char *create_array(unsigned int size, char c);
