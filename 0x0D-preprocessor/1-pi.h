e3c488eb48b58cb8437a7fd190638bfc518a2001
Idris Mustapha <idrismustaphaobadaki777@gmail.com>
2022-11-14 04:29:30 -0800

    1-pi.h

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef 1-PI_H
+#define 1-PI_H
+
+/**
+ * file - 1-pi.h
+ *
+ * desc -  a header file that defines a macro named PI
+ *	as an abbreviation for the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif
