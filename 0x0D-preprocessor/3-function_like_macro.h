cc4a1f6ad89950b201ebd3256d4a67203f85db32
Idris Mustapha <idrismustaphaobadaki777@gmail.com>
2022-11-14 04:46:39 -0800

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * file: 3-function_like_macro.h.
+ *
+ * desc: function-like macro ABS(x) that computes
+ *	the absolute value of a number x.
+ */
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif
