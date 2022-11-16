6ce75b8d32977126d54ca24140ecc7b7872bf00b
Idris Mustapha <idrismustaphaobadaki777@gmail.com>
2022-11-16 09:12:33 -0800

    1-array_iterator.c

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/**
+ * file: function_pointers.h
+ * desc: Header file containing prototypes for all functions
+ *       used in the 0x0F-function_pointers directory.
+ */
+
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+#endif
