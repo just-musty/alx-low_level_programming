bfbd40b064b16d7ff291982a45b0fcf8f6797364
Idris Mustapha <idrismustaphaobadaki777@gmail.com>
2022-11-17 11:54:47 -0800

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+/**
+ * file: variadic_functions_h
+ * desc: Header file containing prototypes for all functions
+ *       used in the 0x0F-variadic_functions directory.
+ */
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
