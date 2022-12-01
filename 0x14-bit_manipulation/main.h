f9b117ebd65e309f5ee6023f7703f4ed9faea7ab
Idris Mustapha <idrismustaphaobadaki777@gmail.com>
2022-12-01 09:19:02 -0800

    100-get_endianness.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,6 +7,7 @@
  *       written in the 0x14-bit_manipulation directory.
  */
 
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
